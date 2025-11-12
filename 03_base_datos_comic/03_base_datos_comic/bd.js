b1fe593c59deeb21d0e043b171ffc452ffa9fd84
Juan jose polo <polomercadojuan06@gmail.com>
2025-11-12 17:15:01 -0500

    Add files via upload

---
+++ 03_base_datos_comic/03_base_datos_comic/bd.js
@@ -0,0 +1,43 @@
+let comic = {
+  "nombrecomic": "un legado que resuena en el presente",
+  "numeroCapitulos": 3,
+  "genero": "ciencia ficcion",
+  "sinopsis": "",
+  "autores": "hinca",
+  "portadaComic": "img/portada.jpg",
+  "years": "2025",
+  "capitulos": [
+    { 
+      "id": 1,
+      "nombre": "El Nacimiento de un Eco Musical",
+      "personajes": ["rodolfo", "abuela"],
+      "portada": "img/portada1.jpg",
+      "descripcion": "El capítulo describe los inicios musicales de Rodolfo Aicardi (Marco Tulio Aicardi Rivera) en su natal Galeras, Sucre, su traslado a Medellín a los 15 años, y su ascenso hasta convertirse en un ícono de la música bailable colombiana con grupos como Los Hispanos y su orquesta La Típica RA7.",
+    },
+    {
+      "id": 2,
+      "nombre": "El Alma detrás de la Voz Inmortal",
+      "personaje": ["rodolfo", ""],
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "id": 3,
+      "nombre": "Un Legado que Resuena en el Presente",
+      "personaje": ["rodolfo", "mateo"],
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+
