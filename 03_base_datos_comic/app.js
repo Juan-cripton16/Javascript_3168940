1ea3d6a550a25eed7fe6c0bb37682d058767f5c3
Juan jose polo <polomercadojuan06@gmail.com>
2025-11-20 17:22:19 -0500

    Add files via upload

--- 03_base_datos_comic/app.js
+++ 03_base_datos_comic/app.js
@@ -1,19 +1,35 @@
 
-const personajes = document.querySelector('.container-personajes');
+const personajes = document.querySelector('.container-personaje');
+const capitulos = document.querySelector('.container-capitulo');
 
 
-
-comic.personajes.forEach(char=> {
+comic.personajes.forEach(char => {
        const div = document.createElement('div');
        div.classList.add('personaje');
+       div.classList.add('descripcion-personaje');
        div.innerHTML = `
-              <div class="imagen">
-          <img src="${char.imagen}" alt="">
+           <div class="imagen">
+          <img src="${char.imagen}" alt="Rodolfo Aicardi">
         </div>
-        <h3 class="${char.nombre}"></h3>
-        <p class="${char.descripcion}">
-          
-        </p>
+        <h3>${char.nombre}</h3>
+        <p>${char.descripcion}</p>
        `;
        personajes.appendChild(div);
 });          
+
+
+comic.capitulos.forEach(cap => {
+       const div = document.createElement('div');
+       div.classList.add('capitulo');
+       div.innerHTML = `
+        <div class="imagen">
+          <img src="${cap.portada}" alt="">
+        </div>
+        <div class="info-capitulo">
+          <h3>${cap.nombre}</h3>
+          <p>${cap.descripcion}</p>
+          <button class="vercapitulo">ver capitulo</button>
+        </div>
+       `;
+       capitulos.appendChild(div);
+}      );
